afe84945dd19d1bfc2a1595ba0dabbfccc805943
Marieza20 <mariezact@hotmail.com>
2025-02-17 15:47:48 -0600

    AlmacenarLocalstorage

--- src/js/funcion.js
+++ src/js/funcion.js
@@ -1,13 +1,12 @@
 const casilla = document.getElementsByClassName("casilla");
 const mensajes = document.getElementById("mensajes");
 const reiniciar = document.getElementById("reiniciar");
+const contG = document.getElementById("contG");
 // Variable para validar si es el turno del jugador
 let jugador = true;
-// Variable para validar si la partida terminó
-let ganaPierdeEmpata = true;
 // Lista que almacena las posiciones jugadas
 let ocupadas = [];
-// Posiciones de gane
+// Combinaciones de gane
 const combinaciones = [
     [0,1,2],
     [3,4,5],
@@ -18,26 +17,42 @@ const combinaciones = [
     [0,4,8],
     [2,4,6]
 ]
+
+let partidasGanadas = JSON.parse(localStorage.getItem("partidasGanadas")) || [];
+// Variable para crear etiqueta p en la página
+let p = document.createElement("p");
+// Función para borrar mensaje
+function borraMensaje(){
+    p.innerHTML = "";
+}
 // Función para validar quién gana la partida
-function partidas() {
+function ganaPierdeEmpata(){
     for(comb of combinaciones){
         if (casilla[comb[0]].textContent == "O" && casilla[comb[1]].textContent == "O" && casilla[comb[2]].textContent == "O"){
-            let p = document.createElement("p");
+            for (let index = 0; index < casilla.length; index++) {
+                casilla[index].style.pointerEvents="none";
+            }
             p.innerText = "Oh no, perdiste :(";
             mensajes.appendChild(p);
-        }else if (casilla[comb[0]].textContent == "X" && casilla[comb[1]].textContent == "X" && casilla[comb[2]].textContent == "X") {
-            let p = document.createElement("p");
+        }else if (casilla[comb[0]].textContent == "X" && casilla[comb[1]].textContent == "X" && casilla[comb[2]].textContent == "X"){
+            for (let index = 0; index < casilla.length; index++) {
+                casilla[index].style.pointerEvents="none";
+            }
             p.innerText = "¡Ganaste la partida!";
             mensajes.appendChild(p);
         }
     }
+    if (ocupadas.length == 9) {
+        p.innerText = "Empate";
+        mensajes.appendChild(p);
+    }
 }
 // Función para que la computadora juegue en una casilla aleatoria
-function pc() {
+function pc(){
     for (let index = 0; index < ocupadas.length; index++) {
         let random = Math.floor(Math.random()*9);
-        if (random != ocupadas[index]) {
-            if (casilla[random].textContent == "") {
+        if (random != ocupadas[index]){
+            if (casilla[random].textContent == ""){
                 casilla[random].textContent = "O";
                 ocupadas.push(random);
                 break;
@@ -50,14 +65,31 @@ for (let index = 0; index < casilla.length; index++){
     const element = casilla[index];
     element.addEventListener("click",function(){
         if (jugador == true){
-            if (element.textContent == "") {
+            if (element.textContent == ""){
                 element.textContent = "X";
                 ocupadas.push(index);
                 pc();
-                partidas();
+                ganaPierdeEmpata();
+                imprimir(partidasGanadas, ganadas)
             }else{
                 alert("¡Ups! La casilla ya fue seleccionada");
             }
         }
     })
+    reiniciar.addEventListener("click",function(){
+        element.textContent = "";
+        for (let index = 0; index < casilla.length; index++) {
+            casilla[index].style.pointerEvents="auto";
+        }
+        borraMensaje();
+    })
+}
+function imprimir(partidasGanadas) {
+    // Muestra el contador en pantalla
+    partidasGanadas.forEach((element) => {
+        // Inserta conteo en la etiqueta creada
+        contG.innerHTML= element.length
+        partidasGanadas.push(element);
+        localStorage.setItem("partidasGanadas",JSON.stringify(partidasGanadas));
+    });
 }
